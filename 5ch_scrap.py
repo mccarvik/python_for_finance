fcd681aab50397936773aa31b7d362a9c7d7b60e
mccarvik <mccarviks@gmail.com>
2017-05-13 13:41:41 +0000

    update to ch5

--- 5ch_scrap.py
+++ 5ch_scrap.py
@@ -19,7 +19,41 @@ def one_dims():
     plt.savefig(PATH + 'cumsum.png', dpi=300)
     plt.close()
     
-
+    x = range(len(y))
+    plt.plot(y.cumsum())
+    plt.grid(True)
+    plt.axis('tight')
+    plt.xlim(-1, 20)
+    plt.ylim(np.min(y.cumsum()) - 1,
+            np.max(y.cumsum()) + 1)
+    plt.savefig(PATH + 'cumsum_grid.png', dpi=300)
+    plt.close()
+    
+def two_dims():
+    np.random.seed(2000)
+    y = np.random.standard_normal((20,2)).cumsum(axis=0)
+    y[:, 0] = y[:, 0] * 100
+    fig, ax1 = plt.subplots()
+    # plt.figure(figsize=(7,4))
+    plt.plot(y[:, 0], 'b', lw=1.5, label='1st')
+    # ro = red dots
+    plt.plot(y[:, 0], 'ro')
+    plt.legend(loc=8)
+    plt.axis('tight')
+    plt.xlabel('index')
+    plt.ylabel('value 1st')
+    plt.title('A Simple Plot')
+    
+    ax2 = ax1.twinx()
+    plt.plot(y[:,1], 'g', lw=1.5, label='2nd')
+    plt.plot(y[:, 1], 'ro')
+    plt.legend(loc=0)
+    plt.ylabel('value 2nd')
+    # plt.grid(True)
+    plt.savefig(PATH + 'two_dim_grid.png', dpi=300)
+    plt.close()
 
 if __name__ ==  "__main__":
-    one_dims()
+    two_dims()
+    
+
