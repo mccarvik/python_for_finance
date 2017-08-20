87ebef4c2cba5e8ff801ca4e81b9f440498233f8
mccarvik <mccarviks@gmail.com>
2017-08-20 00:43:34 +0000

    making progress

--- 14ch_scrap.py
+++ 14ch_scrap.py
@@ -1,43 +1,149 @@
-import pdb
-import seaborn as sns; sns.set()
+import sys, pdb
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
+sys.path.append("/usr/local/lib/python3.4/dist-packages")
 import matplotlib as mpl
-mpl.rcParams['font.family'] = 'serif'
-import http.client as clt
-import urllib.request
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import pdb, ftplib, http, http.client
+import numpy as np
+import pandas as pd
+import seaborn as sns; sns.set()
+import urllib, warnings, time, requests
+import plotly.plotly as py
+import cufflinks as cf
+from pandas_datareader.data import DataReader
+import datetime as dt
+from jinja2 import Template
 
+PATH = '/home/ubuntu/workspace/python_for_finance/png/ch14/'
+
+def web_basics_ftplib():
+    ftp = ftplib.FTP('quant-platform.com')
+    print(ftp.login(user='python', passwd='python'))
+    np.save('./data/array', np.random.standard_normal((100, 100)))
+    f = open('./data/array.npy', 'r')
+    print(ftp.storbinary('STOR array.npy', f))
+    print(ftp.retrlines('LIST'))
+    f = open('./data/array_ftp.npy', 'wb').write
+    print(ftp.retrbinary('RETR array.npy', f))
+    print(ftp.delete('array.npy'))
+    print(ftp.retrlines('LIST'))
+    ftp.close()
+    ftps = ftplib.FTP_TLS('quant-platform.com')
+    print(ftps.login(user='python', passwd='python'))
+    print(ftps.prot_p())
+    print(ftps.retrlines('LIST'))
+    ftps.close()
 
 def web_basics_http():
-    http = clt.HTTPConnection('hilpisch.com')
-    http.request('GET', '/index.htm')
-    resp = http.getresponse()
+    h = http.client.HTTPConnection('hilpisch.com')
+    h.request('GET', '/index.htm')
+    resp = h.getresponse()
     print(resp.status, resp.reason)
     content = resp.read()
     print(content[:100])
+    # first 100 characters of the file
     index = content.find(b' E ')
     print(index)
     print(content[index:index + 29])
-    http.close()
+    h.close()
 
+# Doesnt work any more as yahoo no longer provides this service but urllib use case still valid
 def web_basics_urllib():
-    pdb.set_trace()
     url = 'http://ichart.finance.yahoo.com/table.csv?g=d&ignore=.csv'
     url += '&s=YHOO&a=01&b=1&c=2014&d=02&e=6&f=2014'
-    connect = urllib.request.urlopen(url)
+    connect = urllib.urlopen(url)
     data = connect.read()
     print(data)
     url = 'http://ichart.finance.yahoo.com/table.csv?g=d&ignore=.csv'
     url += '&%s'  # for replacement with parameters
     url += '&d=06&e=30&f=2014'
-    params = urllib.parse.urlencode({'s': 'MSFT', 'a': '05', 'b': 1, 'c': 2014})
+    params = urllib.urlencode({'s': 'MSFT', 'a': '05', 'b': 1, 'c': 2014})
     print(params)
     print(url % params)
-    connect = urllib.request.urlopen(url % params)
+    connect = urllib.urlopen(url % params)
     data = connect.read()
     print(data)
-    print(urllib.request.urlretrieve(url % params, './data/msft.csv'))
+    print(urllib.urlretrieve(url % params, './data/msft.csv'))
     csv = open('./data/msft.csv', 'r')
     csv.readlines()[:5]
 
+def web_plotting_plots():
+    # static
+    start = dt.datetime(2010, 1, 1)
+    end = dt.datetime(2013, 1, 27)
+    data=DataReader("MSFT", 'google', start, end)
+    data = data.reset_index()
+    fig, ax = plt.subplots()
+    data.plot(x='Date', y='Close', grid=True, ax=ax)
+    plt.savefig(PATH + 'MSFT.png', dpi=300)
+    
+    # interactive - may not work in cloud 9
+    warnings.simplefilter('ignore')
+    py.sign_in('Python-Demo-Account', 'gwt101uhh0')
+    # to interactive D3.js plot
+    py.iplot_mpl(fig)
+    # direct approach with Cufflinks
+    data.set_index('Date')['Close'].iplot(world_readable=True)
+
+def web_plotting_realtime():
+    pdb.set_trace()
+    # Site now needs log in
+    url = 'http://analytics.quant-platform.com:12500/prices'
+    # real-time FX (dummy!) data from JSON API
+    api = requests.get(url)
+    print(api)
+    data = api.json()
+    print(data)
+    print(data['tick']['ask'])
+    ticks = pd.DataFrame({'bid': data['tick']['bid'],
+                      'ask': data['tick']['ask'],
+                      'instrument': data['tick']['instrument'],
+                      'time': pd.Timestamp(data['tick']['time'])},
+                      index=[pd.Timestamp(data['tick']['time']),])
+    # initialization of ticks DataFrame
+    print(ticks[['ask', 'bid', 'instrument']])
+    
+    # URL also no longer valid
+    url1 = 'http://www.netfonds.no/quotes/posdump.php?'
+    url2 = 'date=%s%s%s&paper=%s.N&csv_format=csv'
+    url = url1 + url2
+    # must be a business day
+    today = dt.datetime.now()
+    y = '%d' % today.year
+    # current year
+    m = '%02d' % today.month
+    # current month, add leading zero if needed
+    d = '%02d' % today.day
+    # current day, add leading zero if needed
+    sym = 'NKE'
+    print(y, m, d, sym)
+    urlreq = url % (y, m, d, sym)
+    print(urlreq)
+    data = pd.read_csv(urlreq, parse_dates=['time'])
+    # initialize DataFrame object
+    print(data.info())
+
+def templating():
+    print('%d, %d, %d' % (1, 2, 3))
+    print('{}, {}, {}'.format(1, 2, 3))
+    print('{}, {}, {}'.format(*'123'))
+    templ = '''<!doctype html>
+        Just print out <b>numbers</b> provided to the template.
+        <br><br>
+        {% for number in numbers %}
+            {{ number }}
+        {% endfor %}
+        '''
+    t = Template(templ)
+    html = t.render(numbers=range(5))
+    print(html)
+
+
 if __name__ == '__main__':
+    # web_basics_ftplib()
     # web_basics_http()
-    web_basics_urllib()
+    # web_basics_urllib()
+    # web_plotting_plots()
+    # web_plotting_realtime()
+    templating()
---
+++ tradechat/application.py
@@ -0,0 +1,103 @@
+import os, pdb
+import datetime as dt
+
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'tradechat.db'),
+    DEBUG=True,
+    SECRET_KEY='secret_key',
+    ))
+
+app.config.from_envvar('TC_Settings', silent=True)
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+    
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('tables.sql', mode='r') as f:
+            # pdb.set_trace()
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlige_db'):
+        g.sqlite_db.close()
+        
+@app.route('/')
+def show_entries():
+    db = get_db()
+    query = 'select comment, user, time from comments order by id desc'
+    cursor = db.execute(query)
+    comments = cursor.fetchall()
+    return render_template('show_entries.html', comments=comments)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        db = get_db()
+        if request.form['username'] == '' or request.form['password'] == '':
+            error = 'provide both a username and a password'
+        else:
+            db.execute('insert into users (name, password) values (?,?)',
+                [request.form['username'], request.form['password']])
+            db.commit()
+            session['logged_in'] = True
+            flash('You were successfully registered')
+    
+        app.config.update(dict(USERNAME=request.form['username']))
+        return redirect(url_for('show_entries'))
+    return render_template('register.html', error=error)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error=None
+    if request.method == 'POST':
+        db = get_db()
+        try:
+            query = 'select id from users where name = ? and password = ?'
+            id = db.execute(query, (request.form['username'],
+                                    request.form['password'])).fetchone()[0]
+            session['logged_in'] = True
+            flash('You are now logged in')
+            app.config.update(dict(USERNAME=request.form['username']))
+            return redirect(url_for('show_entries'))
+        except:
+            error = "User not found or wrong password"
+    return render_template('login.html', error=error)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    now = dt.datetime.now()
+    db.exectue('insert into comments (comment, user, time) values (?,?,?)',
+            [request.form['text'], app.config['USERNAME'], str(now)[:-7]])
+    db.commit()
+    flash('Your comment was successfully added')
+    return redirect(url_for('show_entries'))
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in, None')
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=8080)
