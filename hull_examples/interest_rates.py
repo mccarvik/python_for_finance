2cddd144d5e0afbfab45705907155fe23938a32b
mccarvik <mccarviks@gmail.com>
2018-01-08 18:41:20 +0000

    calcing par yield and bond yeild

--- hull_examples/interest_rates.py
+++ hull_examples/interest_rates.py
@@ -23,12 +23,21 @@ def convert_to_continuous(r, prds):
 
 def convert_from_continuous(r, prds):
     return prds * (np.exp(r/prds) - 1)
+
+# Calculates the bond yield for a bond to equal its market price
+def calc_bond_yield(px, settle_dt, mat_dt, cpn, prds):
+    pass
+
+# Calculates the coupon a bond would need to trade at par given a yield curve
+def calc_par_yield(par, settle_dt, mat_dt, prds):
+    pass
     
 
 
 if __name__ == '__main__':
-    print(compounding_conversion(.10, 2, 2))
-    print(compounding_conversion(.10, 2, cont=True))
-    print(convert_to_continuous(.10, 2))
-    print(convert_from_continuous(.08, 4))
-    
+    # print(compounding_conversion(.10, 2, 2))
+    # print(compounding_conversion(.10, 2, cont=True))
+    # print(convert_to_continuous(.10, 2))
+    # print(convert_from_continuous(.08, 4))
+    print(calc_bond_yield(98.39, dt.datetime(2015,1,1), dt.datetime(2017,1,1), 6, 2))
+    print(calc_par_yield(100, dt.datetime(2015,1,1), dt.datetime(2017,1,1), 2))
