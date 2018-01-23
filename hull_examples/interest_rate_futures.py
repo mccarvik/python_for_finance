4d29f27e3fe0d37f00b2d73d98e5f1d9b8b121bb
mccarvik <mccarviks@gmail.com>
2018-01-23 21:40:36 +0000

    chunking thru

--- hull_examples/interest_rate_futures.py
+++ hull_examples/interest_rate_futures.py
@@ -17,3 +17,21 @@ import datetime as dt
 yields = [0.01, 0.05, 0.04, 0.01]
 dates = [dt.datetime(2015,1,1), dt.datetime(2015,4,1), dt.datetime(2015,10,1), dt.datetime(2017, 1, 1)]
 dsr = deterministic_short_rate('dsr', list(zip(dates, yields)))
+
+
+def eurodollar_futures_price(quote):
+    # 25$ per basis point move
+    return 10000 * (100 - 0.25 *(100 - quote))
+
+
+# Used to account for the difference between the forward and futures rate
+def convexity_adjustment(fut_rat, sigma, settle_dt, T1, T2):
+    # sigma = st_dev of short term interest rate in 1 year
+    T1 = get_year_deltas([settle_dt, T1])[-1]
+    T2 = get_year_deltas([settle_dt, T2])[-1]
+    return fut_rat - (0.5) * sigma**2 * T1 * T2
+
+
+if __name__ == "__main__":
+    print(eurodollar_futures_price(99.725))
+    print(convexity_adjustment(0.06038, 0.012, dt.datetime(2015,1,1), dt.datetime(2023,1,1), dt.datetime(2023,4,1)))
