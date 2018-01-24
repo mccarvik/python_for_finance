46106557c360a9603fdf79e64018c92cde02feca
mccarvik <mccarviks@gmail.com>
2018-01-24 17:45:52 +0000

    finished chapter, on to swaps

--- hull_examples/interest_rate_futures.py
+++ hull_examples/interest_rate_futures.py
@@ -14,8 +14,8 @@ import numpy as np
 import datetime as dt
 
 
-yields = [0.01, 0.05, 0.04, 0.01]
-dates = [dt.datetime(2015,1,1), dt.datetime(2015,4,1), dt.datetime(2015,10,1), dt.datetime(2017, 1, 1)]
+yields = [0.01, 0.048, 0.04, 0.01]
+dates = [dt.datetime(2015,1,1), dt.datetime(2016,1,5), dt.datetime(2016,10,1), dt.datetime(2017, 1, 1)]
 dsr = deterministic_short_rate('dsr', list(zip(dates, yields)))
 
 
@@ -32,6 +32,21 @@ def convexity_adjustment(fut_rat, sigma, settle_dt, T1, T2):
     return fut_rat - (0.5) * sigma**2 * T1 * T2
 
 
+def next_bootstrap_rate(prev_fwd_rt, settle_dt, last_fwd_dt, next_fwd_dt):
+    num = prev_fwd_rt * get_year_deltas([last_fwd_dt, next_fwd_dt])[-1] + \
+            dsr.get_interpolated_yields([settle_dt, last_fwd_dt])[-1][1] * get_year_deltas([settle_dt, last_fwd_dt])[-1]
+    den = get_year_deltas([settle_dt, next_fwd_dt])[-1]
+    return num / den
+    
+
+# Number of contracts required to hedge against an uncertain change in yld
+def duration_based_hedging(fwd_px, dur_port, dur_fut, cont_px):
+    return (fwd_px * dur_port) / (cont_px * dur_fut)
+    
+
+
 if __name__ == "__main__":
-    print(eurodollar_futures_price(99.725))
-    print(convexity_adjustment(0.06038, 0.012, dt.datetime(2015,1,1), dt.datetime(2023,1,1), dt.datetime(2023,4,1)))
+    # print(eurodollar_futures_price(99.725))
+    # print(convexity_adjustment(0.06038, 0.012, dt.datetime(2015,1,1), dt.datetime(2023,1,1), dt.datetime(2023,4,1)))
+    # print(next_bootstrap_rate(0.053, dt.datetime(2015,1,1), dt.datetime(2016,2,5), dt.datetime(2016,5,6)))
+    print(duration_based_hedging(10000000, 6.8, 9.2, 93062.50))
