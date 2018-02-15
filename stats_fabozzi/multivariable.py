bc807b61be07aa3e3cb92145d5613e466d76d5b4
mccarvik <mccarviks@gmail.com>
2018-02-15 13:32:41 +0000

    covariance and correlation calculated

--- stats_fabozzi/multivariable.py
+++ stats_fabozzi/multivariable.py
@@ -23,6 +23,22 @@ def setup_data(data1, data2):
     data['spy_neg'] = data.apply(lambda x: 1 if x['spy_dret'] < 0 else 0, axis=1)
     return data
 
+def covariance(data1, data2):
+    # measure of joint variation --> when both numbers move away from mean simultaneously, creates large values and vice versa
+    mx = data1.mean()
+    my = data2.mean()
+    
+    tot = 0
+    for x, y in zip(data1, data2):
+        tot += (x - mx) * (y - my)
+    return tot / len(data1)
+
+def correlation(data1, data2):
+    # Pearson correlation coefficient. Will always between -1 and 1
+    std1 = data1.std()
+    std2 = data2.std()
+    return covariance(data1, data2) / (std1 * std2)
+
 
 if __name__ == '__main__':
     data1 = pd.read_csv('aapl.csv')
@@ -35,6 +51,10 @@ if __name__ == '__main__':
     data_rets = data.groupby('appl_dret')[['spy_pos', 'spy_neg']].sum()
     
     # Compare reutrns when negative or positive
-    print(data[data.spy_neg==1]['appl_dret'].mean())
-    print(data[data.spy_pos==1]['appl_dret'].mean())
+    # print(data[data.spy_neg==1]['appl_dret'].mean())
+    # print(data[data.spy_pos==1]['appl_dret'].mean())
+    
+    # covariance and correlation
+    print(covariance(data1['aapl'], data2['spy']))
+    print(correlation(data1['aapl'], data2['spy']))
     
