ba913643ea8a826b5f97ab0e92d10a7504ea185d
mccarvik <mccarviks@gmail.com>
2018-04-11 21:54:48 +0000

    need to do pareto and the means and stadard devs for both

--- stats_fabozzi/cont_dists_extreme.py
+++ stats_fabozzi/cont_dists_extreme.py
@@ -23,7 +23,10 @@ def gen_extreme_val_dist(xi):
     # Gumbel dist --> xi = 0
     # Frechet dist --> xi > 0
     # Weibull dist --> xi < 0
-    m = lambda: 0
+    if xi >= 1:
+        m = float('inf')
+    elif xi == 0:
+        m = 0
     s = lambda: 1
     
     def func(x):
@@ -54,12 +57,40 @@ def gen_extreme_val_dist(xi):
     return dist
 
 
+def gen_pareto_dist(beta, xi):
+    # Distribution employed to model large values wich changes well beyond the typical change
+    m = lambda: 0
+    s = lambda: 1
+    
+    def func(x):
+        if x < 0:
+            return 0
+        else:
+            return (1 / beta) * (1 + xi * (x/beta))**(-1-(1/xi))
+                
+    dist = distribution(func, m, s)
+    def cum_dist(low, hi):
+        return integrate.quad(func, low, hi)[0]
+    
+    dist.cum_dist = cum_dist
+    dist.name = "xi=" + str(xi) + " beta=" + str(beta)
+    # plot_density_function([dist], 'normal_dist', [-4,4,100])
+    # plot_cumulative_dist_func([dist], 'normal_cum_dist', [-4,4,100])
+    return dist
+
 
 if __name__ == '__main__':
-    gumbel = gen_extreme_val_dist(0)
-    frechet = gen_extreme_val_dist(0.5)
-    weibull = gen_extreme_val_dist(-0.5)
-    plot_density_function([gumbel, frechet, weibull], 'gen_extreme_val_dist', [-4,4,100])
-    plot_cumulative_dist_func([gumbel, frechet, weibull], 'gen_extreme_val_cum_dist', [-4,4,100])
-    # print(norm.mean())
-    # print(norm.stdev())
+    # gumbel = gen_extreme_val_dist(0)
+    # frechet = gen_extreme_val_dist(0.5)
+    # weibull = gen_extreme_val_dist(-0.5)
+    # plot_density_function([gumbel, frechet, weibull], 'gen_extreme_val_dist', [-4,4,100])
+    # plot_cumulative_dist_func([gumbel, frechet, weibull], 'gen_extreme_val_cum_dist', [-4,4,100])
+    
+    pareto1 = gen_pareto_dist(1, -0.25)
+    pareto2 = gen_pareto_dist(1, 0)
+    pareto3 = gen_pareto_dist(1, 1)
+    plot_density_function([pareto1, pareto2, pareto3], 'pareto_dist', [0,6,100])
+    plot_cumulative_dist_func([pareto1, pareto2, pareto3], 'pareto_cum_dist', [0,6,100])
+    
+    
+    
