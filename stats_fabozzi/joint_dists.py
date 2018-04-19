18eca88d5dacb56c839fd95cebb597bf174d29ce
mccarvik <mccarviks@gmail.com>
2018-04-19 15:27:16 +0000

    got the modeling done for the normal bivariate distribution

--- stats_fabozzi/joint_dists.py
+++ stats_fabozzi/joint_dists.py
@@ -5,6 +5,7 @@ sys.path.append("/usr/local/lib/python3.4/dist-packages")
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import scipy.integrate as integrate
 from math import sqrt, pi, log, e
 
@@ -32,23 +33,27 @@ def joint_dist_example(m1, m2, o1, o2):
 
 def multivar_normal_dist(m, cov_mat):
     k = len(m)   # k = number of variables
-    dens_func = lambda x: (1 /(np.sqrt(np.linalg.det(cov_mat) * (2*pi)**k))) * (np.exp((-1/2) * (x - m).T * np.linalg.inv(cov_mat) * (x - m))) 
+    dens_func = lambda x: (1 /(np.sqrt(np.linalg.det(cov_mat) * (2*pi)**k))) * (np.exp((-1/2) * (x - m).T.dot(np.linalg.inv(cov_mat)).dot(x - m))) 
+    
+    def cum_dist(low, hi):
+        return integrate.quad(dens_func, low, hi)[0]
     
     xs = []
     for i in range(k):
-      xs.append(np.arange(-3, 3, 0.1))
+      xs.append(np.arange(-3, 3.01, 0.05))
     
     # Rest only works for bivariate, more than 2 vairables wont work
     X1, X2 = np.meshgrid(xs[0], xs[1])
     Z = np.zeros(shape=(len(X1), len(X2)))
-    for i in xs[0]:
-        for j in xs[1]:
-            pdb.set_trace()
-            Z[i,j] = dens_func(np.array([i,j]))
-            
+    R = np.sqrt(X1**2 + X2**2)
+    for i in range(len(xs[0])):
+        for j in range(len(xs[1])):
+            x = np.array([[xs[0][i]],[xs[1][j]]])
+            Z[i,j] = dens_func(x)
+
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    surf = ax.plot_surface(X1, X2, Z, linewidth=0, antialiased=False)
     plt.savefig('png/bivariate_dists/' + 'bivariate' + '.png', dpi=300)
     plt.close()
     
@@ -57,5 +62,5 @@ if __name__ == '__main__':
     # print(joint_dist_example(0, 0.25, 0, 1))
     # cov_mat = np.array([[0.25, 0.30], [0.30, 1.00]])
     cov_mat = np.array([[1, 0], [0, 1]])
-    m = np.array([0, 0])
+    m = np.array([[0], [0]])
     multivar_normal_dist(m, cov_mat)
