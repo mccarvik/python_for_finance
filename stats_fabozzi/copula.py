b29a19f2ec884c65ab14c82e38f61a4d9ffde399
mccarvik <mccarviks@gmail.com>
2018-04-30 21:22:16 +0000

    finished a couple of copula, may try to do more later

--- stats_fabozzi/copula.py
+++ stats_fabozzi/copula.py
@@ -21,7 +21,9 @@ import datetime as dt
 
 
 def gaussian_copula(p):
+    # p = correlation between 2 variables
     # d = 2, only 2 distributions to simplify the calculations
+    d = 2
     cov_mat = np.array([[1,00, p], [p, 1.00]])
     
     # Need to do multiple integration
@@ -29,13 +31,12 @@ def gaussian_copula(p):
     
     def dens_func(x1, x2):
         front = 1 / (2 * pi * np.sqrt(1 - p**2))
-        exp = (x1**2 - 2 * p * x1 * x2 + x2**2)
-    
-    dens_func = lambda x: (1 /(np.sqrt(np.linalg.det(cov_mat) * (2*pi)**k))) * (np.exp((-1/2) * (x - m).T.dot(np.linalg.inv(cov_mat)).dot(x - m))) 
+        exp = (x1**2 - 2 * p * x1 * x2 + x2**2) / (2 * (1 - p**2))
+        return front * np.exp(exp)
     
     xs = []
-    for i in range(k):
-      xs.append(np.arange(0, 1, 0.05))
+    for i in range(d):
+      xs.append(np.arange(0.001, 1, 0.05))
     
     # Rest only works for bivariate, more than 2 vairables wont work
     X1, X2 = np.meshgrid(xs[0], xs[1])
@@ -43,9 +44,10 @@ def gaussian_copula(p):
     R = np.sqrt(X1**2 + X2**2)
     for i in range(len(xs[0])):
         for j in range(len(xs[1])):
-            x = np.array([[xs[0][i]],[xs[1][j]]])
-            Z[i,j] = dens_func(x)
-
+            # Z[i,j] = integrate.nquad(dens_func, [[0, xs[0][i]], [0, xs[1][j]]])
+            Z[i,j] = integrate.dblquad(dens_func, 0, xs[0][i], lambda x: 0, lambda x: xs[1][j])[0]
+    
+    pdb.set_trace()
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     surf = ax.plot_surface(X1, X2, Z, linewidth=0, antialiased=False)
@@ -53,9 +55,9 @@ def gaussian_copula(p):
     plt.close()
     
 
-def multivar_tstudent_dist(m, cov_mat, df):
-    k = len(m)   # k = number of variables
-    v = df       # degress of freedom
+def multivar_tstudent_dist(p, v):
+    # p = correlation between 2 variables
+    # v = degress of freedom
     
     def gamma(x):
         # This only works for integers
@@ -63,17 +65,21 @@ def multivar_tstudent_dist(m, cov_mat, df):
         func = lambda t: np.exp(-t) * t**(x-1)
         return integrate.quad(func, 0, float('inf'))[0]
     
-    def dens_func(x):
-        front = gamma((1/2) * (v + k)) / (gamma((1/2)*v) * (pi * v)**(k/2) * np.linalg.det(cov_mat)**(1/2))
-        back = (1 + ((x - m).T.dot(np.linalg.inv(cov_mat)).dot(x - m)) / v)**(-(v+k)/2)
-        return front * back
+    # d = 2, only 2 distributions to simplify the calculations
+    d = 2
+    cov_mat = np.array([[1.00, p], [p, 1.00]])
+    front = gamma(v/2 + 1) / (gamma(v/2) * np.sqrt(np.linalg.det(cov_mat) * (v * pi)**2))
     
-    def cum_dist(low, hi):
-        return integrate.quad(dens_func, low, hi)[0]
+    # Need to do multiple integration
+    # https://docs.scipy.org/doc/scipy/reference/tutorial/integrate.html
+    
+    def dens_func(x1, x2):
+        y = np.array([[x1],[x2]])
+        return (1 + (y.T.dot(np.linalg.inv(cov_mat)).dot(y) / v))**(-1 * (v/2 + 1))
     
     xs = []
-    for i in range(k):
-      xs.append(np.arange(-3, 3.01, 0.05))
+    for i in range(d):
+      xs.append(np.arange(0.001, 1, 0.05))
     
     # Rest only works for bivariate, more than 2 vairables wont work
     X1, X2 = np.meshgrid(xs[0], xs[1])
@@ -81,24 +87,25 @@ def multivar_tstudent_dist(m, cov_mat, df):
     R = np.sqrt(X1**2 + X2**2)
     for i in range(len(xs[0])):
         for j in range(len(xs[1])):
-            x = np.array([[xs[0][i]],[xs[1][j]]])
-            Z[i,j] = dens_func(x)
-
+            # Z[i,j] = integrate.nquad(dens_func, [[0, xs[0][i]], [0, xs[1][j]]])
+            Z[i,j] = front * integrate.dblquad(dens_func, 0, xs[0][i], lambda x: 0, lambda x: xs[1][j])[0]
+    
+    pdb.set_trace()
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     surf = ax.plot_surface(X1, X2, Z, linewidth=0, antialiased=False)
-    plt.savefig('png/bivariate_dists/' + 'bivariate_tstud' + '.png', dpi=300)
+    plt.savefig('png/copulas/' + 't_student_' + str(p) + '.png', dpi=300)
     plt.close()
 
 
 
 if __name__ == '__main__':
-    gaussian_copula(0)
+    # gaussian_copula(0)
     # gaussian_copula(0.5)
     # gaussian_copula(0.95)
     
-    # cov_mat = np.array([[0.25, 0.30], [0.30, 1.00]])
-    # m = np.array([[0.1], [1.6]])
-    # o = np.array([[0.1], [1.6]])
-    # min_var_port(m, o, cov_mat, 2)
+    multivar_tstudent_dist(0, 5)
+    multivar_tstudent_dist(0.5, 5)
+    multivar_tstudent_dist(0.95, 5)
+    
     
