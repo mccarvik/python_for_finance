c68774738e24249cd7055dcb0f8a73f26f6a6b95
mccarvik <mccarviks@gmail.com>
2018-08-16 00:13:29 +0000

    original commit to mastering python for finance

---
+++ mastering_pff/ch1/oo_approach.py
@@ -0,0 +1,13 @@
+# Object orient approach to python
+
+class Greeting(object):
+    def __init__(self, my_greeting):
+        self.my_greeting = my_greeting
+
+    def say_hello(self, name):
+        print("%s %s" % (self.my_greeting, name))
+
+greeting = Greeting("Hello")
+greeting.say_hello("World")
+greeting.say_hello("Dog")
+greeting.say_hello("Cat")
