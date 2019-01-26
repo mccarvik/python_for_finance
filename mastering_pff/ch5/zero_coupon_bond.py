9ec1c19ae005c83f9f9f445a0554707a657ff9c9
mccarvik <mccarviks@gmail.com>
2019-01-26 04:43:19 +0000

    starting ch5, real good stuff

---
+++ mastering_pff/ch5/zero_coupon_bond.py
@@ -0,0 +1,11 @@
+def zero_coupon_bond(par, y, t):
+    """
+    Price a zero coupon bond.
+    
+    Par - face value of the bond.
+    y - annual yield or rate of the bond.
+    t - time to maturity in years.
+    """
+    return par/(1+y)**t
+
+print(zero_coupon_bond(100, 0.05, 5))
