b85b5ec769307ffb424e77c93ed153470081a5d4
mccarvik <mccarviks@gmail.com>
2019-01-30 01:32:29 +0000

    finished ch7

--- mastering_pff/ch7/pymongo_demo.py
+++ mastering_pff/ch7/pymongo_demo.py
@@ -1,9 +1,13 @@
+import pdb
 import datetime as dt
 import pymongo
-
+from bson.objectid import ObjectId
 client = pymongo.MongoClient("localhost", 27017)
 ticks_db = client.ticks_db
 aapl_collection = ticks_db.aapl
+
+# in separate terminal --> mongod --dbpath /data/db --smallfiles
+# to run mongoDB daemon
 
 tick = {"ticker": "aapl",
         "time": dt.datetime(2014, 11, 17, 10, 0, 0),
@@ -14,18 +18,15 @@ tick = {"ticker": "aapl",
         "vol": 1900000}
 
 tick_id = aapl_collection.insert(tick)
-print tick_id
-print ticks_db.collection_names()
+print(tick_id)
+print(ticks_db.collection_names())
 
-print aapl_collection.find_one()
-print aapl_collection.find_one({"time": dt.datetime(2014, 11, 17, 10, 0, 0)})
+print(aapl_collection.find_one())
+print(aapl_collection.find_one({"time": dt.datetime(2014, 11, 17, 10, 0, 0)}))
 
-from bson.objectid import ObjectId
-print aapl_collection.find_one({"_id": \
-	ObjectId("548490486d3ba7178b6c36ba")})
+print(aapl_collection.find_one({"_id": ObjectId("548490486d3ba7178b6c36ba")}))
 
 aapl_collection.remove()
-
 aapl_collection.insert([tick,
                        {"ticker": "aapl",
                         "time": dt.datetime(2014, 11, 17, 10, 1, 0),
@@ -42,18 +43,18 @@ aapl_collection.insert([tick,
                         "last": 115.00,
                         "vol": 2100000}])
 
-print aapl_collection.count()
-print aapl_collection.find({"open": 115.58}).count()
+print(aapl_collection.count())
+print(aapl_collection.find({"open": 115.58}).count())
 
 for aapl_tick in aapl_collection.find():
-   print aapl_tick
+   print(aapl_tick)
 
 cutoff_time = dt.datetime(2014, 11, 17, 10, 2, 0)
 for tick in aapl_collection.find(
        {"time": {"$lt": cutoff_time}}).sort("time"):
-   print tick 
+   print(tick)
 
 sorted_ticks = aapl_collection.find().sort(
     [("time", pymongo.DESCENDING)])
 for tick in sorted_ticks:
-    print tick 
+    print(tick)
