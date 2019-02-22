ee46b174bf5f6a7ba5d5b5350dda237e905ba73e
mccarvik <mccarviks@gmail.com>
2019-02-22 22:23:43 +0000

    getting going

--- research/utils/eod_px_util.py
+++ research/utils/eod_px_util.py
@@ -1,14 +1,19 @@
+"""
+This util will grab data to and from the db with eod prices
+"""
 import sys
-sys.path.append("/home/ubuntu/workspace/python_for_finance")
-sys.path.append("/home/ubuntu/workspace/ml_dev_work")
-sys.path.append("/usr/local/lib/python3.4/dist-packages")
 import datetime as dt
 from dateutil.relativedelta import relativedelta
 import pandas_datareader as dr
-from utils.db_utils import DBHelper, restart
+sys.path.append("/home/ubuntu/workspace/python_for_finance")
+sys.path.append("/home/ubuntu/workspace/ml_dev_work")
+from utils.db_utils import DBHelper
 
 
 def get_time_series(tick, start=None, end=dt.datetime.today()):
+    """
+    Pulls an individual time series from the API
+    """
     if not start:
         start = dt.datetime.today() - relativedelta(years=5)
     data = dr.DataReader(tick, 'iex', start, end)['close']
@@ -16,30 +21,35 @@ def get_time_series(tick, start=None, end=dt.datetime.today()):
 
 
 def load_db():
+    """
+    Gathers px data one by one through the ticks
+    """
     ticks = []
-    with open("/home/ubuntu/workspace//ml_dev_work/utils/snp_ticks_2018_02_13.txt", "r") as f:
-        for line in f:
+    with open("/home/ubuntu/workspace//ml_dev_work/utils/snp_ticks_2018_02_13.txt", "r") as file:
+        for line in file:
             ticks.append(line.strip())
-    
+
     for ind_t in ticks:
         data = get_time_series(ind_t).reset_index()
         data['tick'] = ind_t
         data.columns = ['date', 'px', 'tick']
-        sendToDB(data)
+        send_to_db(data)
 
 
-def sendToDB(df):
-    with DBHelper() as db:
-        db.connect()
+def send_to_db(data):
+    """
+    Sends the px data to the DB
+    """
+    with DBHelper() as dbh:
+        dbh.connect()
         table = 'eod_px'
-        import pdb; pdb.set_trace()
         prim_keys = ['tick', 'date']
-        for ind, vals in df.iterrows():
+        for ind, vals in data.iterrows():
             val_dict = vals.to_dict()
-            db.upsert(table, val_dict, prim_keys)
+            dbh.upsert(table, val_dict, prim_keys)
 
 if __name__ == '__main__':
     # S_DT = dt.datetime(2013, 1, 1)
-    E_DT = dt.datetime(2019, 2, 22)
-    get_time_series('F')
+    # E_DT = dt.datetime(2019, 2, 22)
+    # get_time_series('F')
     load_db()
